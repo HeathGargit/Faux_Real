7b0502bc0624120c85738bac300e02302797b74a
Heath Gargit <Heath Gargit>
2016-04-10 17:19:29 +1000

    Added main menu. added camera rotation on main menu for fanciness

---
+++ Assets/_Scripts/CameraControls/MainMenuCameraPan.cs
@@ -0,0 +1,23 @@
+﻿/*---------------------------------------------------------
+File Name: MainMenuCameraPan.cs
+Purpose: Slowly rotate the main menu's camera to make it look cool and futuristicky
+Author: Heath Parkes (gargit@gargit.net)
+Modified: 20/3/2016
+-----------------------------------------------------------
+Copyright 2016 AIE/HP
+---------------------------------------------------------*/
+
+using UnityEngine;
+using System.Collections;
+
+public class MainMenuCameraPan : MonoBehaviour {
+
+    //initialising the camera rotation speed.
+    private float m_RotationSpeed = 0.0016f;
+
+    //camera rotation is done in the fixedupdate to smooth it's pan. If it was done in normal update the pan speed would change depending on system performance.
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, m_RotationSpeed, 0);
+    }
+}
