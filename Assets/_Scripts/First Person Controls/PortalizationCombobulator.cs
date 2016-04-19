63f11a3399a96470346b87405c569f91a37b8f24
Heath Gargit <Heath Gargit>
2016-04-19 21:35:51 +1000

    Added level 2 and 3. created script for making teleportation possible

---
+++ Assets/_Scripts/First Person Controls/PortalizationCombobulator.cs
@@ -0,0 +1,21 @@
+﻿/*---------------------------------------------------------
+File Name: PortalizationCombobulator.cs
+Purpose: To move the player between portals
+Authors: Heath Parkes (gargit@gargit.net), Liam Ellis (liam.ellis37@gmail.com)
+Modified: 19/4/2016
+-----------------------------------------------------------
+Copyright 2016 LE/HP
+---------------------------------------------------------*/
+
+using UnityEngine;
+using System.Collections;
+
+public class PortalizationCombobulator : MonoBehaviour {
+
+    public GameObject m_TargetPortal;
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = m_TargetPortal.transform.position;
+    }
+}
